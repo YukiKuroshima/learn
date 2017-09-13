a3d86132da547015ad8120403611c4af62749721
YukiKuroshima <yukikuro1993@gmail.com>
2017-09-13 14:59:17 -0700

    :art:

---
+++ graphql-tutorial/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    quoteOfTheDay: String
+    andom: Float!
+    rollDice(numDice: Int!, numSides: Int): [Int]
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+const root = {
+  quoteOfTheDay: () => (
+    Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within'
+  ),
+  random: () => (
+    Math.random()
+  ),
+  rollDice: function ({numDice, numSides}) {
+    var output = [];
+    for (var i = 0; i < numDice; i++) {
+      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+    }
+    return output;
+  }
+};
+
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(4000);
+console.log('Running a GraphQL API server at 4000');
