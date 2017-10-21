95e4173c176dc8f124d7e3ac69310ed8751c74c0
YukiKuroshima <yukikuro1993@gmail.com>
2017-10-20 22:25:44 -0700

    :tada:

--- simple-flask-API/run.py
+++ simple-flask-API/run.py
@@ -1,7 +1,50 @@
 from flask import Flask, jsonify, send_from_directory, request
+from flask_jwt import JWT, jwt_required, current_identity
+from werkzeug.security import safe_str_cmp
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'super-secret'
+app.config['JWT_AUTH_USERNAME_KEY'] = 'email'
 
+
+class User(object):
+    def __init__(self, id, email, password):
+        self.id = id
+        self.email = email
+        self.password = password
+
+    def __str__(self):
+        return "User(id='%s')" % self.id
+
+
+users = [
+        User(1, '1@1.com', 'abcxyz'),
+        User(2, 'user2', 'abcxyz'),
+        ]
+
+email_table = {u.email: u for u in users}
+userid_table = {u.id: u for u in users}
+
+
+def authenticate(email, password):
+    user = email_table.get(email, None)
+    if user and safe_str_cmp(user.password.encode('utf-8'), password.encode('utf-8')):
+        return user
+
+
+def identity(payload):
+    user_id = payload['identity']
+    return userid_table.get(user_id, None)
+
+
+jwt = JWT(app, authenticate, identity)
+
+
+@app.route('/protected')
+@jwt_required()
+def protected():
+    return '%s' % current_identity
 
 # Execute when HTTPrequest calls 'GET /hello'
 # Or
